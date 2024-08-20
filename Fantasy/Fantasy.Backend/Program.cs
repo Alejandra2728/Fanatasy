27cb41954af3fe84732c14ee6c1a3d8f68c59676
Alejandra Fabiana Rubio <rubioalejandraf@gmail.com>
2024-08-20 07:53:04 -0300

    Add countries entity

--- Fantasy/Fantasy.Backend/Program.cs
+++ Fantasy/Fantasy.Backend/Program.cs
@@ -1,3 +1,6 @@
+using Fantasy.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,9 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
